8b2e6c386750d1df1646c9fd9e2bf2a9bd897be3
wonjin0403 <37763659+wonjin0403@users.noreply.github.com>
2020-08-06 15:23:33 +0900

    Update download_and_convert_flowers.py

--- research/slim/datasets/download_and_convert_flowers.py
+++ research/slim/datasets/download_and_convert_flowers.py
@@ -211,5 +211,5 @@ def run(dataset_dir):
       list(zip(list(range(len(class_names))), class_names)))
   dataset_utils.write_label_file(labels_to_class_names, dataset_dir)
 
-  _clean_up_temporary_files(dataset_dir)
+  #_clean_up_temporary_files(dataset_dir)
   print('\nFinished converting the Flowers dataset!')
